a05adaf3888ab83c05712bed2055f700696ee46c
deepuezhap <deepuezhap007@gmail.com>
2025-03-08 12:58:51 +0530

    frontend and loading module

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,26 @@ from models import Base, User, Car  # Ensure Car model is imported
 from routes.auth import auth_router
 from routes.protected_routes import protected_router
 from routes.car import car_router  # Car route already in `routes/`
+from fastapi.middleware.cors import CORSMiddleware
+import time
+
 
 app = FastAPI()
+
+
+# Allow frontend to communicate with backend
+origins = [
+    "http://localhost:5173"  # React Vite Dev Server
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows requests from frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Create database tables on startup
 Base.metadata.create_all(bind=engine)
@@ -21,4 +39,5 @@ app.include_router(car_router, prefix="/cars", tags=["Cars"])
 
 @app.get("/")
 def home():
+    time.sleep(3)  # Simulate a 3-second delay
     return {"message": "Welcome to CarNest API"}

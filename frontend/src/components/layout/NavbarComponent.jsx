93f6a6bdf1a642f833b4b932af039cf11a5a66e2
deepuezhap <deepuezhap007@gmail.com>
2025-03-15 21:33:47 +0530

    useAuth_seperate_file

--- frontend/src/components/layout/NavbarComponent.jsx
+++ frontend/src/components/layout/NavbarComponent.jsx
@@ -1,55 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
-import { jwtDecode } from "jwt-decode";
-
+import useAuth from '../../hooks/useAuth';
 
 const NavbarComponent = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const currentUser = useAuth();
   const navigate = useNavigate();
-
-  const validateToken = (token) => {
-    try {
-      const decodedToken = jwtDecode(token);
-      const currentTime = Date.now() / 1000; // Current time in seconds
-      if (decodedToken.exp > currentTime) {
-        return true;
-      } else {
-        localStorage.removeItem("token"); // Token expired, remove it
-        return false;
-      }
-    } catch (error) {
-      console.error("Token is invalid:", error);
-      localStorage.removeItem("token");
-      return false;
-    }
-  };
-
-  const checkToken = () => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      setIsLoggedIn(validateToken(token));
-    } else {
-      setIsLoggedIn(false);
-    }
-  };
-
-  useEffect(() => {
-    checkToken();
-
-    const interval = setInterval(checkToken, 60000); // Check token every minute
-
-    window.addEventListener('storage', checkToken);
-
-    return () => {
-      clearInterval(interval);
-      window.removeEventListener('storage', checkToken);
-    };
-  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
-    setIsLoggedIn(false);
     navigate("/");
   };
 
@@ -67,7 +26,7 @@ const NavbarComponent = () => {
             <Nav.Link as={Link} to="/about">About Us</Nav.Link>
           </Nav>
           <Nav>
-            {isLoggedIn ? (
+            {currentUser ? (
               <>
                 <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
                 <Button variant="outline-light" onClick={handleLogout}>Logout</Button>
